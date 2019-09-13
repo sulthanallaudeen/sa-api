ce2d4f4f83b128c1c640d3ec073f552ad9faf8f6
sulthanallaudeen <sa@sulthanallaudeen.com>
2019-09-13 15:28:32 +0530

    User related API's Migrated

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,79 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* User Login */
+router.post('/login', function(req, res, next) {
+  dbModel.User.findOne({
+    where: {email: req.body.email},
+    attributes: ['id','name','email', 'password']
+  }).then(userData => {
+    if(userData){
+      bcrypt.compare(req.body.password, userData.dataValues.password).then(function(matched) {
+        if(matched){
+          var token = util.generateToken();
+          delete userData.dataValues.password;
+          userData.dataValues.token = token;
+          updateUserToken(userData.id,token);
+          res.status(200).json({ success: 1, data: userData.dataValues });
+        } else {
+          res.status(200).json({ success: 0, message: 'Wrong Password' });
+        }
+      });
+    } else {
+      res.status(200).json({ success: 2, message: 'User not found' });
+    }
+  })
 });
+
+
+/* Get User Profile */
+router.get('/', function(req, res, next) {
+  dbModel.User.findOne({
+    where: {api_token: req.headers.token},
+    attributes: ['id','name','email','api_token']
+  }).then(userData => {
+    if(userData){
+      res.status(200).json({ success: 1, data: userData.dataValues });
+    } else {
+      res.status(200).json({ success: 2, message: 'User not found' });
+    }
+  })
+});
+
+/* Update Profile */
+router.post('/', function(req, res, next) {
+  dbModel.User.update(
+    { name: req.body.name }, 
+    { where: { api_token : req.headers.token }}
+    ).then(function(done) {
+      res.status(200).json({ success: 1, message: "User profile updated success"});
+  })
+});
+
+/* User Logout */
+router.post('/logout', function(req, res, next) {
+  dbModel.User.findOne({
+    where: {api_token: req.body.token},
+    attributes: ['id','name','email','password']
+  }).then(userData => {
+    if(userData){
+      updateUserToken(userData.id,'');
+      res.status(200).json({ success: 1, message: 'User logged out' });
+    } else {
+      res.status(200).json({ success: 2, message: 'User not found' });
+    }
+  })  
+});
+
+// Util Function
+
+function updateUserToken(userid,token){
+  dbModel.User.update(
+    { api_token: token }, 
+    { where: { id : userid }}
+    ).then(function(done) {
+      // Done
+    })
+}
 
 module.exports = router;
